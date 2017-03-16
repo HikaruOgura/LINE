0e65d4a77cb62321a9b566ade4ece5589aff95c2
小倉輝 <x7deed53bc9k8@gmail.com>
2017-03-16 15:43:03 +0900

    friendコントローラとfriendモデルをつくった

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     has_many :chats
+    has_many :friends
     validates :name,presence:{ message: "ユーザー名が未入力です"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316021713) do
+ActiveRecord::Schema.define(version: 20170316063615) do
 
   create_table "chats", force: :cascade do |t|
     t.integer  "send_to_ID", limit: 4
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20170316021713) do
   end
 
   add_index "chats", ["user_id"], name: "index_chats_on_user_id", using: :btree
+
+  create_table "friends", force: :cascade do |t|
+    t.integer  "friend_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",       limit: 255
